791eb86f669ff576eb7b40e0483511d4922523f3
sahadat96 <sahadatappdev@gmail.com>
2025-08-11 09:59:54 +0600

    Built route processing flow

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo 'hello sahadat';
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+
+$app->router->get('/', function(){
+   return "Wellcome";
+});
+
+$app->router->get('/about', function(){
+   return "About";
+});
+
+
+
+$app->run();
